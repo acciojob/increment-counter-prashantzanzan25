ed2ca1a20e4c4e9f27242a6c754941c16f6d6b27
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-11-19 14:12:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+var counterElement = document.getElementById("counter");
+var incrementBtn = document.getElementById("incrementBtn");
+
+incrementBtn.addEventListener("click", function() {
+	var currentValue = parseInt(counterElement.innerText);
+
+    alert("Un-incremented value: " + currentValue);
+
+    counterElement.innerText = currentValue + 1;
+});
+
