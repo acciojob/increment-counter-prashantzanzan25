c82a6af83474198c704a9c1dbf8d9c74b29cdbd2
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2024-01-08 13:41:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let button = document.getElementById('incrementBtn');
 button.addEventListener('click', function() {
-	let counter = document.getElementById("counter");
-	let count = parseInt(counter.innerText);
-	alert(count);
-	counter.innerText=count x 1;
+			let counter = document.getElementById("counter");
+            let count = parseInt(counter.innerText);
+            alert(count);
+            counter.innerText = count + 1;
 });
-
